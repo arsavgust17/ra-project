e636b09380c68668eea6f9d0c55659bfdde43b32
arsen <arsavgust17@gmail.com>
2019-07-27 17:36:53 +0400

    base-layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MainWindow></MainWindow>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MainWindow from "@/components/main-window.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    MainWindow,
   }
 };
 </script>
