40ef3bcd72cc21ff9d331a7fa5215884fe44cfa9
dmitriy <tsuckanovdmitriy@gmail.com>
2019-07-27 16:59:01 +0300

    qwe

---
+++ src/components/auth.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "auth"
+    }
+</script>
+
+<style scoped>
+.main
+{
+    background-image: url("../../public/images/auth.png");
+}
+</style>
