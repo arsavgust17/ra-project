2c5f16c10249bb6de49023eb064dc3ea2f35280b
arsen <arsavgust17@gmail.com>
2019-07-28 04:24:54 +0400

    include QR

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from "./router";
 import store from "./store";
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 Vue.use(iView);
+Vue.use(axios);
 
 new Vue({
   router,
