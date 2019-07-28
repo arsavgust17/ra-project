201d4045617a8de8be806f256ece21c75f3c596a
dmitriy <tsuckanovdmitriy@gmail.com>
2019-07-28 03:16:10 +0300

    В photoload, при успешной загрузке нужно картинку куда-то девать, ибо пока я просто редирект сделал

--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <Slider v-model="value8" show-input></Slider>
+    <div :class="isMobile ? 'main':'mainmob'">
+
     </div>
 </template>
 
@@ -15,5 +15,27 @@
 >>>.ivu-slider-button
 {
 border: 2px solid darkred;
+}.main
+ {
+     background-color: #467DE4;
+     position: fixed;
+     top: 0;
+     left: 0;
+     width: 100%;
+     height: 100%;
+     display: flex;
+     align-items: center;
+     justify-content: flex-end;
+ }
+.mainmob {
+    background-color: #467DE4;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 }
 </style>
